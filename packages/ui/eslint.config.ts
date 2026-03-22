fff4f14c5df01d44536ed9f236f03979129b04bb
Lanz Conanan <99579095+JeloKuneho@users.noreply.github.com>
2026-03-22 15:43:06 +0000

    generated a temp eslint in packages ui + installed eslint-plugin-react

---
+++ packages/ui/eslint.config.ts
@@ -0,0 +1,22 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+        ecmaVersion: 2020,
+        globals: globals.browser
+    } 
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+]);
