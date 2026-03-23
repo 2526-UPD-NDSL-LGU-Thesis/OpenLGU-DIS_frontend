711b9657230c3723f1ec403d0712e1a6b52c8db4
Lanz Conanan <99579095+JeloKuneho@users.noreply.github.com>
2026-03-23 05:27:56 +0000

    feat: connected to backend and tested ping api via pingService.ts

--- apps/public-portal/src/App.tsx
+++ apps/public-portal/src/App.tsx
@@ -1,7 +1,28 @@
-import { VerificationPage } from "./pages/VerificationPage"
+import { VerificationPage } from "./pages/VerificationPage";
+import ThemeToggle from "./components/ThemeToggle";
+
+import { useEffect, useState } from "react";
+import { pingBackend } from "./services/pingService";
 
 function App() {
-  return <VerificationPage />
+  const [pingStatus, setPingStatus] = useState<string>("Not tested yet")
+
+  return (
+    <>
+      <ThemeToggle />
+      <VerificationPage />
+      <div>
+        <p>Backend Status: {pingStatus}</p>
+        <button
+          onClick={async () => {
+            const result = await pingBackend()
+            setPingStatus(result.message)
+          }}>
+            Ping
+        </button>
+      </div>
+    </>
+  )
 }
 
 export default App
