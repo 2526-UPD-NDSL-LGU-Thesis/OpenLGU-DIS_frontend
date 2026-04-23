4bd1deb1d659501bd164098d9998beadc97e9623
Lanz Conanan <99579095+JeloKuneho@users.noreply.github.com>
2026-04-23 09:00:36 +0000

    👷(public-portal): make vitest detect dev environment variables

--- apps/public-portal/vitest.config.ts
+++ apps/public-portal/vitest.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vitest/config';
+import { loadEnv } from 'vite';
 
 // https://vitest.dev/config/
 export default defineConfig({
     test: {
         setupFiles: ['./src/tests/vitest.setup.ts'],
+        // set-up for using dev environmental variables
+        env: loadEnv("development", process.cwd(), ''),
     },
 })
