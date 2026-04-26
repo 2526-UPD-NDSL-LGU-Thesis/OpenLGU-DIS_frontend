4ac3251760cc37d8dbb10021e9662d6d00dd4602
Lanz Conanan <99579095+JeloKuneho@users.noreply.github.com>
2026-04-26 08:12:40 +0000

    👷(public-portal): set-up MSW for browser dev

--- apps/public-portal/src/main.tsx
+++ apps/public-portal/src/main.tsx
@@ -3,13 +3,30 @@ import "./styles/locals.css";
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx';
-import { ThemeProvider } from './components/theme-provider.tsx';
+import App from '#/App.tsx';
+import { ThemeProvider } from '#/components/theme-provider.tsx';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ThemeProvider defaultTheme="system">
-      <App />
-    </ThemeProvider>
-  </StrictMode>,
-)
+// From https://mswjs.io/docs/integrations/browser#conditionally-enable-mocking
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return
+  }
+ 
+  const { worker } = await import('#/tests/browser.ts')
+ 
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start()
+}
+
+
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <ThemeProvider defaultTheme="system">
+        <App />
+      </ThemeProvider>
+    </StrictMode>,
+  )
+});
+
