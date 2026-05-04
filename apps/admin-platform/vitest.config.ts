34ea85564bd2890a20087fafb82da359942b64c7
Lanz Conanan <llconanan@up.edu.ph>
2026-05-04 03:53:11 +0000

    👷(admin-platform): set-up React Testing Library

--- apps/admin-platform/vitest.config.ts
+++ apps/admin-platform/vitest.config.ts
@@ -4,8 +4,10 @@ import { loadEnv } from 'vite';
 // https://vitest.dev/config/
 export default defineConfig({
     test: {
+        environment: 'jsdom',
         setupFiles: ['./src/tests/vitest.setup.ts'],
         // set-up for using dev environmental variables
         env: loadEnv("development", process.cwd(), ''),
     },
 })
+
